afbe3b99392d2632e7b52c034eba28cd4ea509ba
Ethan Hampton <ethanhampton00@gmail.com>
2023-05-12 14:48:23 -0400

    Home page with basic requirements

--- personal-portfolio/src/Home.jsx
+++ personal-portfolio/src/Home.jsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
-import Goals from './Goals.jsx'
-import Hobbies from './Hobbies.jsx'
-import Introduction from './Introduction.jsx'
+import Goals from './Personal Information/Goals.jsx'
+import Hobbies from './Personal Information/Hobbies.jsx'
+import Introduction from './Personal Information/Introduction.jsx'
 //import './public/description.txt'
 import './Home.css'
+import './profile.jpeg'
 
 export default function Portfolio() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <title>Ethan&aposs Personal Portfolio</title>
+      <title>Ethans Personal Portfolio</title>
+      <header>
+        Home
+      </header>
+      <img src = "profile.jpeg" className="profile"/>
       <h1>Ethan Hampton</h1>
       <h2>Software Intern @ Neudesic</h2>
       <h3>MIS Student @ THE Ohio State University</h3>
-      <h4>Introduction: {Introduction()}</h4>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      <h5>{Goals()}</h5>
-      <h5>{Hobbies()}</h5>
-      </div>
-      <p className="read-the-docs">
-        This is a test
+      <div>{Introduction()}</div>
+      <div>{Goals()}</div>
+      <div>{Hobbies()}</div>
+      <iframe src="https://giphy.com/embed/11qCjC856PSmnm" width="480" height="327" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <p><a href="https://giphy.com/gifs/genius-albums-11qCjC856PSmnm">via GIPHY</a></p>
+      <p className="footer">
+        There's nothing exciting here, I promise.
       </p>
     </>
   )
--- personal-portfolio/src/Personal Information/Goals.jsx
+++ personal-portfolio/src/Personal Information/Goals.jsx
@@ -4,9 +4,9 @@ export default function Goals() {
         <h1>Goals</h1>
         <p>My goals are:</p>
         <ul>To be a learning sponge and soak up as must knowlege as possible</ul>
-        <ul>To be prepared fo a transition to a full-time salaried employee</ul>
+        <ul>To be prepared for a transition to a full-time salaried employee</ul>
         <ul>To be hired on at Neudesic post internship</ul>
-        <ul>To be capable of building a full scale react project wihtout help</ul>
+        <ul>To be capable of building a full scale react project without help</ul>
       </>
     )
   }
