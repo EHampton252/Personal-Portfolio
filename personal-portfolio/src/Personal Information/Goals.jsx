37b3e894cacf132f9fd966690bbe3555256d2fc4
Ethan Hampton <ethan.hampton@neudesic.com>
2023-05-16 15:36:19 -0400

    fixes formatting for goals and hobbies

--- personal-portfolio/src/Personal Information/Goals.jsx
+++ personal-portfolio/src/Personal Information/Goals.jsx
@@ -1,12 +1,14 @@
 export default function Goals() {
     return (
       <>
-        <header className="goals-header">Goals</header>
+        <header className="goals-header">GOALS</header>
         <p>My goals are:</p>
-        <ul className="goal-list">To be a learning sponge and soak up as must knowlege as possible</ul>
-        <ul className="goal-list">To be prepared for a transition to a full-time salaried employee</ul>
-        <ul className="goal-list">To be hired on at Neudesic post internship</ul>
-        <ul className="goal-list">To be capable of building a full scale react project without help</ul>
+        <ul className="goal-list">
+          <li>To be a learning sponge and soak up as must knowlege as possible</li>
+          <li>To be prepared for a transition to a full-time salaried employee</li>
+          <li>To be hired on at Neudesic post internship</li>
+          <li>To be capable of building a full scale react project without help</li>
+        </ul>
       </>
     )
   }
--- personal-portfolio/src/Personal Information/Hobbies.jsx
+++ personal-portfolio/src/Personal Information/Hobbies.jsx
@@ -1,11 +1,13 @@
 export default function Hobbies() {
     return (
       <>
-        <h1>Hobbies</h1>
-        <p>Powerlifting</p>
-        <p>Video Games</p>
-        <p>Reading</p>
-        <p>Coding my own side projects</p>
+        <h1 className="hobby-header">HOBBIES</h1>
+        <ul className="hobby-list">
+          <li>Powerlifting</li>
+          <li>Video Games</li>
+          <li>Reading</li>
+          <li>Coding my own side projects</li>
+        </ul>
       </>
     )
   }
